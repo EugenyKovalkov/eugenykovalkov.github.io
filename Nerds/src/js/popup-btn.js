106507c087086e6d1c5fe49dae66de857d68f0b4
EugenyKovalkov <55461050+EugenyKovalkov@users.noreply.github.com>
2019-10-31 15:31:29 +0300

    Add files via upload

---
+++ Nerds/src/js/popup-btn.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+          $('.popup-btn').on('click', function(event){
+            event.preventDefault();
+          $('.popup').fadeIn();
+          });
+          $('.popup-close').on('click', function(event){
+            event.preventDefault();
+            $('.popup').fadeOut();
+          })});  
