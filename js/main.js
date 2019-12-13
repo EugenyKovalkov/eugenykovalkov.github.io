6a8226b4d085aafbf7979dbc1c97fb06231cdcd8
EugenyKovalkov <eugenykovalkov@gmail.com>
2019-12-13 16:23:11 +0400

    feat: add js modal for popup window

--- js/main.js
+++ js/main.js
@@ -1,33 +1,58 @@
-// connect pop-up windiw
-$(document).ready(function () {
-  var modal = $('.modal'),
-      modalBtn = $('[data-toggle=modal]'),
-      closeBtn = $('.modal__close');
-    
-  modalBtn.on('click', function () {
-    modal.toggleClass('modal--visible');
-  });
-  closeBtn.on('click', function () {
-    modal.toggleClass('modal--visible');    
+document.addEventListener("DOMContentLoaded", function (event) {
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  };
+
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
   });
 
-  var mySwiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  document.body.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.matches('.modal__close') || target.matches('#modal--window')) {
+      switchModal();
+    }
   });
-  var next = $('.swiper-button-next');
-  var prev = $('.swiper-button-prev');
-  var bullets = $('.swiper-pagination');
 
-  next.css('left', prev.width() + 37 + bullets.width() + 37)
-  bullets.css('left', prev.width() + 37)
+  document.addEventListener('keyup', (event) => {
+    if (event.code === 'Escape') {
+      modal.classList.remove('modal--visible');
+    }
+  });
 });
+
+// connect pop-up windiw
+// $(document).ready(function () {
+//   var modal = $('.modal'),
+//       modalBtn = $('[data-toggle=modal]'),
+//       closeBtn = $('.modal__close');
+    
+//   modalBtn.on('click', function () {
+//     modal.toggleClass('modal--visible');
+//   });
+//   closeBtn.on('click', function () {
+//     modal.toggleClass('modal--visible');    
+//   });
+
+//   var mySwiper = new Swiper('.swiper-container', {
+//     loop: true,
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: 'bullets',
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
+//   var next = $('.swiper-button-next');
+//   var prev = $('.swiper-button-prev');
+//   var bullets = $('.swiper-pagination');
+
+//   next.css('left', prev.width() + 37 + bullets.width() + 37)
+//   bullets.css('left', prev.width() + 37)
+// });
 
 
