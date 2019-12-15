4260ba4f6772a0fb98dd266a2d1e3b0d7373da4a
EugenyKovalkov <eugenykovalkov@gmail.com>
2019-12-15 05:54:19 +0400

    feat:add section project

--- js/main.js
+++ js/main.js
@@ -23,36 +23,24 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
 });
 
-// connect pop-up windiw
-// $(document).ready(function () {
-//   var modal = $('.modal'),
-//       modalBtn = $('[data-toggle=modal]'),
-//       closeBtn = $('.modal__close');
-    
-//   modalBtn.on('click', function () {
-//     modal.toggleClass('modal--visible');
-//   });
-//   closeBtn.on('click', function () {
-//     modal.toggleClass('modal--visible');    
-//   });
+$(document).ready(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
 
-//   var mySwiper = new Swiper('.swiper-container', {
-//     loop: true,
-//     pagination: {
-//       el: '.swiper-pagination',
-//       type: 'bullets',
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
-//   var next = $('.swiper-button-next');
-//   var prev = $('.swiper-button-prev');
-//   var bullets = $('.swiper-pagination');
-
-//   next.css('left', prev.width() + 37 + bullets.width() + 37)
-//   bullets.css('left', prev.width() + 37)
-// });
+  next.css('left', prev.width() + 37 + bullets.width() + 37)
+  bullets.css('left', prev.width() + 37)
+});
 
 
